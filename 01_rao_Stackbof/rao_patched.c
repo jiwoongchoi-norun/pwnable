26365c852bce0522caa4f2115a8eac9f63c253c2
jiwoongchoi-norun <jw020308@naver.com>
2025-12-31 19:07:55 +0900

    feat: add rao stack bof analysis

---
+++ 01_rao_Stackbof/rao_patched.c
@@ -0,0 +1,31 @@
+// Name: rao.c
+// Compile: gcc -o rao rao.c -fno-stack-protector -no-pie
+
+#include <stdio.h>
+#include <unistd.h>
+
+void init()
+{
+    setvbuf(stdin, 0, 2, 0);
+    setvbuf(stdout, 0, 2, 0);
+}
+
+void get_shell()
+{
+    char *cmd = "/bin/sh";
+    char *args[] = {cmd, NULL};
+
+    execve(cmd, args, NULL);
+}
+
+int main()
+{
+    char buf[0x28];
+
+    init();
+
+    printf("Input: ");
+    scanf("%39s", buf); // no canary, but limit input size to prevent overflow
+
+    return 0;
+}
